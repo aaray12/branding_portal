78535727f35d1a0656667e2001b90a2ed3a634b3
Aaron Ray <aaray619@gmail.com>
2021-10-12 14:40:19 -0500

    heroku test

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://aaron:abiyoyo12@branding-portal.6mzdk.mongodb.net/branding-portal?retryWrites=true&w=majority",
+  mongoURI: "mongodb+srv://aray:abi619@branding-portal.6mzdk.mongodb.net/branding-portal?retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
